3befcb3adb6b620ba3f7dd3029330ad031f21acd
andreamusso96 <andreamusso96@gmail.com>
2025-04-22 18:11:00 +0200

    added zonal statistics

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from traffic_data.enums import City, Service, TrafficType
 from datetime import date, time
 from aggregate_night_traffic import get_night_traffic_city_service
 from rasterize_traffic import rasterize_traffic_city_service_by_tile_time
+from zonal_statistics import compute_zonal_statistics_traffic_raster_city_service
 import plotly.express as px
 
 def test_load_traffic():
@@ -61,9 +62,33 @@ def test_rasterize_traffic():
     fig.show()
 
 
+def test_zonal_statistics():
+    city = City.BORDEAUX
+    traffic_type = TrafficType.UL_AND_DL
+    start_night = time(22)
+    end_night = time(5)
+    service = Service.WIKIPEDIA
+    remove_nights_before_holiday_and_anomalies = True
 
+    night_traffic = get_night_traffic_city_service(city=city, traffic_type=traffic_type, start_night=start_night, end_night=end_night, service=service, remove_nights_before_holiday_and_anomalies=remove_nights_before_holiday_and_anomalies)
 
+    raster = rasterize_traffic_city_service_by_tile_time(traffic_data=night_traffic, city=city)
+    print('RASTER DATA')
+    print(raster.dims)
+    print(raster.shape)
+    print(raster.coords)
+    print(raster.head())
+    print('RASTER DATA')
+
+    raster_2230 = raster.sel(time="22:30:00")
+    raster_2230_pd = raster_2230.squeeze().to_pandas()
+    fig = px.imshow(raster_2230_pd, color_continuous_scale='Viridis', title="Traffic Service and Geo Test", labels={'value': 'Value'}, width=800, height=600,  origin='lower')
+    fig.show()
+
+    zonal_stats = compute_zonal_statistics_traffic_raster_city_service(city=city, service=service, traffic_raster=raster, vectors=night_traffic, vector_id_col='tile', coverage_threshold=0.8)
+    print('ZONAL STATISTICS')
+    print(zonal_stats.head())
 
 
 if __name__ == '__main__':
-    test_rasterize_traffic()
+    test_zonal_statistics()
---
+++ zonal_statistics.py
@@ -0,0 +1,58 @@
+from typing import List
+
+import geopandas as gpd
+import rasterio
+import numpy as np
+import xarray as xr
+import rasterstats
+import shapely
+import pandas as pd
+from datetime import time
+
+from traffic_data.enums import City, Service
+
+
+def compute_zonal_statistics_traffic_raster_city_service(city: City, service: Service, traffic_raster: xr.DataArray, vectors: gpd.GeoDataFrame, vector_id_col: str, coverage_threshold: float) -> pd.DataFrame:
+    assert traffic_raster.ndim == 3, "The input raster must be 3D, i.e. it must correspond to the traffic for a specific city, service pairs. The dimensions should be y,x and time."
+    vectors = extract_vector_within_raster_coverage(raster=traffic_raster.isel(time=0), vector=vectors, coverage_threshold=coverage_threshold)
+    zonal_stats = [_get_zonal_statistics(c=city, s=service, t=t, raster=traffic_raster.sel(time=t), vectors=vectors, vector_id_col=vector_id_col) for t in traffic_raster.time.values]
+    zonal_stats = pd.concat(zonal_stats)
+    return zonal_stats
+
+
+def _get_zonal_statistics(c: City, s: Service, t: time, raster: xr.DataArray, vectors: gpd.GeoDataFrame, vector_id_col: str) -> pd.DataFrame:
+    zonal_stats = extract_zonal_stats(raster=raster, no_data=np.nan, vector=vectors, stats=['mean'], all_touched=True)
+    zonal_stats['service'] = s.value
+    zonal_stats['time'] = t
+    zonal_stats['city'] = c.value
+    zonal_stats.rename(columns={'mean_raster_value': 'traffic'}, inplace=True)
+    zonal_stats = zonal_stats[[vector_id_col, 'city', 'service', 'time', 'traffic']].copy()
+    return zonal_stats
+
+
+def extract_zonal_stats(raster: xr.DataArray, no_data: float, vector: gpd.GeoDataFrame, stats: List[str] = ['mean'], all_touched: bool = True) -> pd.DataFrame:
+    _check_xarray_is_2d_raster(raster=raster)
+    vector['tmp_index'] = np.arange(len(vector))
+    raster_statistics = rasterstats.zonal_stats(vectors=vector.geometry, raster=raster.values, affine=raster.rio.transform(), nodata=no_data, stats=stats, all_touched=all_touched)
+    raster_statistics = pd.DataFrame([[r[k] for k in stats] for r in raster_statistics], index=np.arange(len(vector)), columns=[f'{s}_raster_value' for s in stats])
+    vector_with_zonal_stats = gpd.GeoDataFrame(pd.merge(vector, raster_statistics, left_on='tmp_index', right_index=True, how='left'))
+    vector_with_zonal_stats.drop(columns='tmp_index', inplace=True)
+    # Returns null for vectors that do not intersect the raster
+    return vector_with_zonal_stats
+
+
+def extract_vector_within_raster_coverage(raster: xr.DataArray, vector: gpd.GeoDataFrame, coverage_threshold: float = 0.8) -> gpd.GeoDataFrame:
+    _check_xarray_is_2d_raster(raster=raster)
+    binary_raster_np = np.where(np.isnan(raster.values), 0, 1).astype(rasterio.uint8)
+    level_curve_vector = gpd.GeoDataFrame([{'geometry': shapely.geometry.shape(s), 'value': v} for s, v in rasterio.features.shapes(binary_raster_np, mask=None, transform=raster.rio.transform())], crs=raster.rio.crs)
+    level_curve_vector['geometry'] = level_curve_vector['geometry'].buffer(1)
+    raster_coverage = level_curve_vector[level_curve_vector.value == 1].unary_union
+    vector = vector.loc[vector.intersection(raster_coverage).area / vector.area > coverage_threshold].copy()
+    return vector
+
+
+def _check_xarray_is_2d_raster(raster: xr.DataArray):
+    assert raster.ndim == 2, "The input raster must be 2D"
+    assert raster.rio is not None, "The input raster must have a CRS"
+    assert raster.rio.transform() is not None, "The input raster must have a transform"
+    assert raster.rio.crs is not None, "The input raster must have a CRS"
