8cd24885674594e5783404d5d02218abb4e94b33
andreamusso96 <andreamusso96@gmail.com>
2025-11-10 11:37:52 +0100

    timedelta issue

--- aggregate_traffic_to_hours.py
+++ aggregate_traffic_to_hours.py
@@ -7,6 +7,7 @@ import xarray as xr
 import numpy as np
 from datetime import time, datetime, timedelta
 import logging
+from datetime import time
 
 from traffic_data.enums import City, Service, TrafficType, TimeOptions
 from traffic_data.load import load_traffic_data_city
@@ -26,5 +27,5 @@ def get_traffic_city_service_hour(city: City, service: Service, traffic_type: Tr
     traffic_data_service = load_traffic_data_city(traffic_type=traffic_type, city=city, service=[service], day=TimeOptions.get_days())
     traffic_data_service = traffic_data_service.sum(dim='day')
     traffic_data_service = traffic_data_service.squeeze('service').to_pandas()
-    traffic_data_service = traffic_data_service.rename(columns={t: t.strftime('%H:%M:%S') for t in traffic_data_service.columns})
+    traffic_data_service = traffic_data_service.rename(columns={t: time(t.components.hours, t.components.minutes, t.components.seconds).strftime('%H:%M:%S') for t in traffic_data_service.columns})
     return traffic_data_service
